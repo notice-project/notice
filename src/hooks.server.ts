d2ebaec9ec0cbf5aae8e09fd4dd9090183bfa11d
alan910127 <70696274+alan910127@users.noreply.github.com>
2023-12-30 14:40:06 +0800

    ✨ update auth hooks to use authjs

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,19 +1,25 @@
-import { building } from "$app/environment";
-import { redirect, type Handle } from "@sveltejs/kit";
+import {
+  GOOGLE_OAUTH_CLIENT_ID,
+  GOOGLE_OAUTH_CLIENT_SECRET,
+  NYCU_OAUTH_CLIENT_ID,
+  NYCU_OAUTH_CLIENT_SECRET,
+} from "$env/static/private";
+import NYCU from "$lib/auth/providers/nycu";
+import Google from "@auth/core/providers/google";
+import { DrizzleAdapter } from "@auth/drizzle-adapter";
+import { SvelteKitAuth } from "@auth/sveltekit";
+import { db } from "./server/db";
 
-export const handle: Handle = async ({ event, resolve }) => {
-  if (building) {
-    return resolve(event);
-  }
-
-  const sessionId = event.cookies.get("sessionId");
-
-  if (!sessionId && event.url.pathname !== "/sign-in") {
-    redirect(301, "/sign-in");
-  }
-  // get user info from db
-
-  const response = await resolve(event);
-
-  return response;
-};
+export const handle = SvelteKitAuth({
+  adapter: DrizzleAdapter(db),
+  providers: [
+    Google({
+      clientId: GOOGLE_OAUTH_CLIENT_ID,
+      clientSecret: GOOGLE_OAUTH_CLIENT_SECRET,
+    }),
+    NYCU({
+      clientId: NYCU_OAUTH_CLIENT_ID,
+      clientSecret: NYCU_OAUTH_CLIENT_SECRET,
+    }),
+  ],
+});
