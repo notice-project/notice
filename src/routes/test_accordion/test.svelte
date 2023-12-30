bd9b44b7cfd5d3c10b862a06d01a8dd2bf842b76
yrlin411 <wn4doe.cs09@nycu.edu.tw>
2023-12-30 20:20:43 +0800

    ✨ test modifying the bookshelf into accordions

---
+++ src/routes/test_accordion/test.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+  import * as Accordion from "$lib/components/ui/accordion";
+
+  let { selected, noteTitle, count } = $props<{
+    selected?: boolean;
+    noteTitle: string;
+    noteNames: string[];
+    count: number;
+  }>();
+</script>
+
+<Accordion.Root
+  class="flex flex-col items-center justify-center bg-white px-16 py-12 max-md:px-5"
+>
+  <Accordion.Item value="Book 1">
+    <Accordion.Trigger class="text-xl font-bold text-neutral-700">
+      {noteTitle}
+    </Accordion.Trigger>
+
+    {#each noteNames as noteName (noteName)}
+      <Accordion.Content>
+        <button>
+          <div class="text-xl font-bold text-neutral-700">{noteName}</div>
+        </button>
+      </Accordion.Content>
+    {/each}
+  </Accordion.Item>
+</Accordion.Root>
