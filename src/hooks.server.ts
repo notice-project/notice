7f98aac90eec45770e1e09433e2ad1306851e026
alan910127 <70696274+alan910127@users.noreply.github.com>
2023-12-30 15:34:57 +0800

    🚨 use dynamic variables for client ID and secrets

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,25 +1,22 @@
-import {
-  GOOGLE_OAUTH_CLIENT_ID,
-  GOOGLE_OAUTH_CLIENT_SECRET,
-  NYCU_OAUTH_CLIENT_ID,
-  NYCU_OAUTH_CLIENT_SECRET,
-} from "$env/static/private";
+import { env } from "$env/dynamic/private";
 import NYCU from "$lib/auth/providers/nycu";
 import Google from "@auth/core/providers/google";
 import { DrizzleAdapter } from "@auth/drizzle-adapter";
 import { SvelteKitAuth } from "@auth/sveltekit";
 import { db } from "./server/db";
 
-export const handle = SvelteKitAuth({
-  adapter: DrizzleAdapter(db),
-  providers: [
-    Google({
-      clientId: GOOGLE_OAUTH_CLIENT_ID,
-      clientSecret: GOOGLE_OAUTH_CLIENT_SECRET,
-    }),
-    NYCU({
-      clientId: NYCU_OAUTH_CLIENT_ID,
-      clientSecret: NYCU_OAUTH_CLIENT_SECRET,
-    }),
-  ],
+export const handle = SvelteKitAuth(async () => {
+  return {
+    adapter: DrizzleAdapter(db),
+    providers: [
+      Google({
+        clientId: env.GOOGLE_OAUTH_CLIENT_ID,
+        clientSecret: env.GOOGLE_OAUTH_CLIENT_SECRET,
+      }),
+      NYCU({
+        clientId: env.NYCU_OAUTH_CLIENT_ID,
+        clientSecret: env.NYCU_OAUTH_CLIENT_SECRET,
+      }),
+    ],
+  };
 });
