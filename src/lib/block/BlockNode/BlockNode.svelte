1c96cc687451680e878fdd646dcb331e1b4cbe1a
Cheng-Hsun Wang <103883300+BaBa0525@users.noreply.github.com>
2023-12-30 16:09:05 +0800

    ✨ note functionality (#17)

---
+++ src/lib/block/BlockNode/BlockNode.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import type { BlockNodeClass } from "./types.svelte";
+
+  type BlockNodeProps = {
+    node: BlockNodeClass;
+  };
+
+  let { node } = $props<BlockNodeProps>();
+
+  $effect(() => {
+    if (node.inputRef) {
+      node.inputRef.focus();
+    }
+  });
+</script>
+
+<input
+  type="text"
+  class="border"
+  bind:this={node.inputRef}
+  bind:value={node.value}
+  onkeydown={(e) => node.keydownHandler(e)}
+/>
---
+++ src/routes/test/+page.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import BlockNode from "$lib/block/BlockNode/BlockNode.svelte";
+  import { BlockNodeClass } from "$lib/block/BlockNode/types.svelte";
+  import { onMount } from "svelte";
+
+  let rootNode = new BlockNodeClass("root", [], null);
+
+  onMount(() => {
+    rootNode.appendChild("");
+    rootNode = rootNode;
+  });
+
+  $: console.log("rootNode: ", rootNode.children);
+</script>
+
+<h1>This is test page</h1>
+
+<div class="flex flex-col pl-4">
+  {#each rootNode.children as childNode (childNode.id)}
+    <BlockNode node={childNode} />
+  {/each}
+</div>
