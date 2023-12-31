5ed1b539398273a0783345d2c096082c782bf059
alan910127 <70696274+alan910127@users.noreply.github.com>
2024-01-01 01:03:53 +0800

    💄 add landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,29 +1,39 @@
 <script lang="ts">
   import { page } from "$app/stores";
-  import { signOut } from "@auth/sveltekit/client";
-
-  let name = $state("Not!ce");
+  import * as Button from "$lib/components/ui/button";
+  import NoticeIcon from "$lib/icons/notice.svelte";
 </script>
 
-<div class="flex h-full flex-col items-center justify-center gap-6">
-  <h1 class="text-5xl font-semibold tracking-tight text-gray-600">
-    Welcome to {name}
-  </h1>
-  <p>Here is nothing. Go develop something!</p>
-  <div>
-    <label for="name">Project Name:</label>
-    <input type="text" bind:value={name} class="border" />
+<main
+  class="container flex h-full w-full flex-col items-center justify-center gap-16"
+>
+  <div class="flex flex-col items-center gap-4">
+    <NoticeIcon width={128} height={128} />
+    <h1 class="text-5xl font-bold tracking-tighter">Not!ce</h1>
+    <p class="text-xl font-semibold">
+      <span
+        class="bg-gradient-to-tr from-[#100c0c] to-[#ffe372] bg-clip-text text-3xl font-extrabold tracking-tight text-transparent"
+      >
+        AI-powered
+      </span>
+      note-taking app that helps you take notes faster and more efficiently.
+    </p>
   </div>
 
-  <!-- eslint-disable-next-line svelte/valid-compile -->
-  {#if $page.data.session?.user}
-    <pre>{JSON.stringify($page.data.session.user, null, 2)}</pre>
-    <button onclick={() => signOut()}>Logout</button>
+  <div class="flex justify-center gap-4">
+    {#if $page.data.session?.user}
+      <Button.Root href="/bookshelf">My Notes</Button.Root>
+    {:else}
+      <Button.Root href="/sign-in">Login</Button.Root>
+    {/if}
 
-    <form method="post">
-      <button formaction="?/testCookies">Test Cookies</button>
-    </form>
-  {:else}
-    <a href="/sign-in">Sign In</a>
-  {/if}
-</div>
+    <Button.Root
+      href="https://github.com/notice-project/notice"
+      target="_blank"
+      rel="noopener noreferrer"
+      variant="outline"
+    >
+      Source Code
+    </Button.Root>
+  </div>
+</main>
