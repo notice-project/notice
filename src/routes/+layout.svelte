1740516f01c6da0716f2bab9ca2cd6695d723e59
alan910127 <70696274+alan910127@users.noreply.github.com>
2024-01-01 01:04:29 +0800

    🔧 setup tanstack query

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,19 @@
 <script lang="ts">
+  import { browser } from "$app/environment";
+  import { QueryClient, QueryClientProvider } from "@tanstack/svelte-query";
+  import { SvelteQueryDevtools } from "@tanstack/svelte-query-devtools";
   import "../app.css";
+
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        enabled: browser,
+      },
+    },
+  });
 </script>
 
-<slot />
+<QueryClientProvider client={queryClient}>
+  <slot />
+  <SvelteQueryDevtools />
+</QueryClientProvider>
