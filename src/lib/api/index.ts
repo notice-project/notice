ab45ca24d14ad4c2981a9de0a671960b8ad85986
alan910127 <70696274+alan910127@users.noreply.github.com>
2024-01-01 01:01:00 +0800

    🏷️ generate api types with openapi-ts

---
+++ src/lib/api/index.ts
@@ -0,0 +1,8 @@
+import { env } from "$env/dynamic/public";
+import createClient from "openapi-fetch";
+import type { paths } from "./schema";
+
+export const api = createClient<paths>({
+  baseUrl: env.PUBLIC_API_URL,
+  credentials: "include",
+});
