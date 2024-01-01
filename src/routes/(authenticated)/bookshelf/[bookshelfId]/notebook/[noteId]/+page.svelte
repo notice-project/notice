208f916c05706816e6bc5573f2cef88319a97339
Cheng-Hsun Wang <103883300+BaBa0525@users.noreply.github.com>
2024-01-02 05:38:01 +0800

    ✨ microphone api (#30)

--- src/routes/(authenticated)/bookshelf/[bookshelfId]/notebook/[noteId]/+page.svelte
+++ src/routes/(authenticated)/bookshelf/[bookshelfId]/notebook/[noteId]/+page.svelte
@@ -6,6 +6,7 @@
   import * as Button from "$lib/components/ui/button";
   import Mic from "$lib/icons/Mic.svelte";
   import Micoff from "$lib/icons/Micoff.svelte";
+  import { onMount } from "svelte";
   import type { PageData } from "./$types";
 
   type NoteMessage = {
@@ -18,20 +19,19 @@
   }>();
 
   let rootNode = $state<RootNodeClass | null>(null);
-  let isMicOn = $state(false);
 
-  let ws: WebSocket;
+  let noteWS: WebSocket;
   $effect(() => {
-    ws = new WebSocket(
+    noteWS = new WebSocket(
       `${env.PUBLIC_WS_URL}/bookshelves/${data.props.bookshelfId}/notes/${data.props.noteId}/ws`,
     );
-    ws.onopen = () => {
-      ws.send(
+    noteWS.onopen = () => {
+      noteWS.send(
         JSON.stringify({ type: "init", payload: data.props.sessionToken }),
       );
     };
 
-    ws.onmessage = (event) => {
+    noteWS.onmessage = (event) => {
       let message = JSON.parse(event.data) as NoteMessage;
       if (message.type === "note") {
         rootNode = RootNodeClass.load(message.payload);
@@ -39,7 +39,7 @@
     };
 
     return () => {
-      ws.close();
+      noteWS.close();
     };
   });
 
@@ -50,7 +50,7 @@
       }
 
       if (rootNode.updateTitle) {
-        ws.send(
+        noteWS.send(
           JSON.stringify({
             type: "update title",
             payload: rootNode.value,
@@ -64,7 +64,7 @@
       if (rootNode.updateAll) {
         const payload = rootNode.dumpAll();
 
-        ws.send(
+        noteWS.send(
           JSON.stringify({
             type: "update all",
             payload: payload.children,
@@ -84,7 +84,7 @@
           continue;
         }
 
-        ws.send(
+        noteWS.send(
           JSON.stringify({
             type: "update",
             payload: {
@@ -103,13 +103,77 @@
       clearInterval(intervalId);
     };
   });
+
+  let isMicOn = $state(false);
+  let microphoneWS = $state<WebSocket | null>(null);
+
+  onMount(() => {
+    microphoneWS = new WebSocket(
+      `${env.PUBLIC_WS_URL}/bookshelves/${data.props.bookshelfId}/notes/${data.props.noteId}/transcription/ws`,
+    );
+
+    microphoneWS.onopen = () => {
+      console.log("microphoneWS opened!");
+      microphoneWS?.send(
+        JSON.stringify({ type: "init", payload: data.props.sessionToken }),
+      );
+    };
+
+    return () => {
+      microphoneWS?.close();
+    };
+  });
+
+  let mediaRecorder = $state<MediaRecorder | null>(null);
+  onMount(() => {
+    const genMediaRecorder = async () => {
+      const media = await navigator.mediaDevices.getUserMedia({ audio: true });
+      mediaRecorder = new MediaRecorder(media);
+      mediaRecorder.ondataavailable = (e) => {
+        microphoneWS?.send(e.data);
+      };
+      mediaRecorder.onstop = () => {
+        microphoneWS?.send(
+          JSON.stringify({
+            type: "stop",
+          }),
+        );
+      };
+    };
+
+    genMediaRecorder();
+
+    return () => {
+      mediaRecorder?.stop();
+    };
+  });
 </script>
 
+<div class="flex w-full justify-center">
+  <div class="mx-10 flex w-full max-w-4xl flex-col">
+    {#if rootNode}
+      <RootNode node={rootNode} />
+    {:else}
+      <div class="text-2xl font-bold">Loading...</div>
+    {/if}
+  </div>
+</div>
+
 {#if rootNode}
-  <RootNode node={rootNode} />
-{:else}
-  <div class="flex h-full items-center justify-center">
-    <div class="text-2xl font-bold">Loading...</div>
+  <div class="flex w-full items-center justify-center">
+    <div class="mt-8 flex w-full max-w-4xl items-center justify-center">
+      <div class="ml-4 flex-grow border-t border-[#D9D9D9]" />
+      <Button.Root
+        variant="outline"
+        class=" w-full max-w-lg rounded-none border-[#D9D9D9] text-[#B0B0B0] hover:bg-[#D9D9D9] hover:text-white"
+        onclick={() => {
+          // noop
+        }}
+      >
+        Notice Me!
+      </Button.Root>
+      <div class="mr-4 flex-grow border-t border-[#D9D9D9]" />
+    </div>
   </div>
 {/if}
 
@@ -118,6 +182,22 @@
   size="icon"
   class="absolute bottom-8 right-8 rounded-full"
   onclick={() => {
+    if (isMicOn) {
+      mediaRecorder?.stop();
+      microphoneWS?.send(
+        JSON.stringify({
+          type: "stop",
+        }),
+      );
+    } else {
+      mediaRecorder?.start(1000);
+      microphoneWS?.send(
+        JSON.stringify({
+          type: "start",
+        }),
+      );
+    }
+
     isMicOn = !isMicOn;
   }}
 >
