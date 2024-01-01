9547b643de3b3e5c5bbec2aca0e54dbe12f39139
BaBa0525 <103883300+BaBa0525@users.noreply.github.com>
2024-01-02 01:40:49 +0800

    ✨ add microphone

--- src/routes/(authenticated)/bookshelf/[bookshelfId]/notebook/[noteId]/+page.svelte
+++ src/routes/(authenticated)/bookshelf/[bookshelfId]/notebook/[noteId]/+page.svelte
@@ -1,8 +1,26 @@
 <script lang="ts">
   import RootNode from "$lib/block/RootNode/RootNode.svelte";
   import { RootNodeClass } from "$lib/block/RootNode/types.svelte";
+  import * as Button from "$lib/components/ui/button";
+  import Mic from "$lib/icons/Mic.svelte";
+  import Micoff from "$lib/icons/Micoff.svelte";
 
   let rootNode = new RootNodeClass({ value: "", children: [] });
+  let isMicOn = $state(false);
 </script>
 
 <RootNode node={rootNode} />
+<Button.Root
+  variant="outline"
+  size="icon"
+  class="absolute bottom-8 right-8 rounded-full"
+  onclick={() => {
+    isMicOn = !isMicOn;
+  }}
+>
+  {#if isMicOn}
+    <Mic />
+  {:else}
+    <Micoff />
+  {/if}
+</Button.Root>
